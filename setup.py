2719b184dbf6d4076c5d53956e6bf8cf737fa59a
RobertJN <robertjnies@gmail.com>
2022-06-07 16:43:15 -0400

    force new explorerhat version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def install():
     os.system("sudo raspi-config nonint do_i2c 0")
 
     print("Installing explorerhat lib")
-    os.system("sudo apt-get install python3-explorerhat")
+    os.system("sudo pip3 install explorerhat")
 
     print("Installing flask")
     os.system("sudo pip install flask")
